884773d1152fafd1fd4986ab1262c3e465c91425
Benedikt Bender <benediktbender@gmail.com>
2018-11-24 18:27:38 +0100

    first functioning chrome extension

---
+++ chromeExtension/script.js
@@ -0,0 +1,27 @@
+var createButton = function (postUrl) {
+    var button = document.createElement("a");
+    button.type = "button";
+    button.text = "Add to Mangalib";
+    button.classList.add("btn-comments");
+    button.onclick = function () {
+        $.post("http://localhost:8080/mangas", postUrl, function(data, status){
+            console.log("after");
+        });
+    };
+    return button;
+}
+url = window.location.href;
+if(url.includes("/manga/")){
+    var container = document.getElementsByClassName("detailtopbtn")[0];
+    var comment = document.getElementsByClassName("btn-comments")[0];
+    container.removeChild(comment);
+    container.appendChild(createButton(url));
+}else{
+    var listings = document.getElementsByClassName("cover-info");
+    for(var index in listings){
+        var listing = listings[index];
+        var href = listing.getElementsByClassName("title")[0].getElementsByTagName("a")[0].href;
+        listing.appendChild(createButton(href));
+    }
+}
+
--- src/main/java/es/heavensgat/mangalib/server/controllers/RestController.java
+++ src/main/java/es/heavensgat/mangalib/server/controllers/RestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.net.URLDecoder;
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     @RequestMapping("/mangas")
@@ -15,6 +17,10 @@ public class RestController {
 
     @PostMapping("/mangas")
     public void crawlManga(@RequestBody String mangaUrl) {
-        MangaUtil.crawlCompleteManga(mangaUrl);
+        String url = mangaUrl;
+        if(mangaUrl.contains("%2f") || mangaUrl.contains("%2F")){
+            url = URLDecoder.decode(mangaUrl.contains("=&") ? mangaUrl.split("=&")[0] : mangaUrl);
+        }
+        MangaUtil.crawlCompleteManga(url);
     }
 }
