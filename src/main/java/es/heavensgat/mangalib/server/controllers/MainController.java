3f343e676cbe9178fa1f37d82155dbe5a17ebf69
Benedikt Bender <bbender@ukaachen.de>
2018-11-26 16:16:03 +0100

    implemented file size meta information

--- src/main/java/es/heavensgat/mangalib/server/controllers/MainController.java
+++ src/main/java/es/heavensgat/mangalib/server/controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,9 +20,11 @@ public class MainController {
     @RequestMapping(value = "/files")
     public void getFile(@RequestParam(value = "file_name") String fileName, HttpServletResponse response) {
         try{
-            InputStream is = new FileInputStream(MangaUtil.BASE_DIRECTORY + "/mangas/" + fileName.replace(" ", "_").replace(".pdf", "") + "/" + fileName);
+            File file = new File(MangaUtil.BASE_DIRECTORY + "/mangas/" + fileName.replace(" ", "_").replace(".pdf", "") + "/" + fileName);
+            InputStream is = new FileInputStream(file);
             response.setContentType("application/pdf");
             response.setHeader("Content-disposition", "attachment;filename=" + fileName);
+            response.addHeader("Content-Length", "" + file.length());
             IOUtils.copy(is, response.getOutputStream());
             response.flushBuffer();
         } catch (IOException ex) {
